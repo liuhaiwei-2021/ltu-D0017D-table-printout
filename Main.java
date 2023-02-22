1a534d923f6ef66de9a9dc98f75a1fc2170b8401
Haiwei Liu <80680154+liuhaiwei-2021@users.noreply.github.com>
2023-02-22 08:57:12 +0000

    assign hardcoded numbers ( 1000, 3) to constant

--- Main.java
+++ Main.java
@@ -15,8 +15,10 @@ class Main {
     final double VOLTAGE_230 = 230;
     final double VOLTAGE_400 = 400;
     final double BATTERRY_CAPACITY = 35.8;
+    final int SQUARE_ROOT_3 = 3;
+    final int W2KW = 1000;
 
-    // Declare varibles for current (3 such values)
+    // Declare constants for current (3 such values)
     final double CURRENT_10 = 10.0;
     final double CURRENT_16 = 16.0;
     final double CURRENT_32 = 32.0;
@@ -29,27 +31,27 @@ class Main {
     // Caculate all the charging power and charging time
     double chargingPower1 = 0.0;
     double chargingTime1 = 0.0;
-    chargingPower1 = Math.round((CURRENT_10 * VOLTAGE_230) / 1000 * scale) / scale;
+    chargingPower1 = Math.round((CURRENT_10 * VOLTAGE_230) / W2KW * scale) / scale;
     chargingTime1 = Math.round((BATTERRY_CAPACITY / chargingPower1) * scale) / scale;
 
     double chargingPower2 = 0.0;
     double chargingTime2 = 0.0;
-    chargingPower2 = Math.round((CURRENT_16 * VOLTAGE_230) / 1000 * scale) / scale;
+    chargingPower2 = Math.round((CURRENT_16 * VOLTAGE_230) / W2KW * scale) / scale;
     chargingTime2 = Math.round((BATTERRY_CAPACITY / chargingPower2) * scale) / scale;
 
     double chargingPower3 = 0.0;
     double chargingTime3 = 0.0;
-    chargingPower3 = Math.round((CURRENT_10 * VOLTAGE_400 * Math.sqrt(3)) / 1000 * scale) / scale;
+    chargingPower3 = Math.round((CURRENT_10 * VOLTAGE_400 * Math.sqrt(SQUARE_ROOT_3)) / W2KW * scale) / scale;
     chargingTime3 = Math.round((BATTERRY_CAPACITY / chargingPower3) * scale) / scale;
 
     double chargingPower4 = 0.0;
     double chargingTime4 = 0.0;
-    chargingPower4 = Math.round((CURRENT_16 * VOLTAGE_400 * Math.sqrt(3)) / 1000 * scale) / scale;
+    chargingPower4 = Math.round((CURRENT_16 * VOLTAGE_400 * Math.sqrt(SQUARE_ROOT_3)) / W2KW * scale) / scale;
     chargingTime4 = Math.round((BATTERRY_CAPACITY / chargingPower4) * scale) / scale;
 
     double chargingPower5 = 0.0;
     double chargingTime5 = 0.0;
-    chargingPower5 = Math.round((CURRENT_32 * VOLTAGE_400 * Math.sqrt(3)) / 1000 * scale) / scale;
+    chargingPower5 = Math.round((CURRENT_32 * VOLTAGE_400 * Math.sqrt(SQUARE_ROOT_3)) / W2KW * scale) / scale;
     chargingTime5 = Math.round((BATTERRY_CAPACITY / chargingPower5) * scale) / scale;
 
     // Print out table
